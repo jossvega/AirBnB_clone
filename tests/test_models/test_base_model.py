c9df9003b6bb9febcbe666d55255ae7d0512d1c9
Jocelyn Vega <jossvega103@gmail.com>
2020-11-04 19:32:21 +0000

    "Add new test and modify BseModel fix typo"

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,34 +4,41 @@
 import unittest
 import os
 from models.base_model import BaseModel
-from datetime import datetime
+import pep8
 
 
 class TEST_base(unittest.TestCase):
+    """this will test the base model class"""
 
     @classmethod
-    def setup(self):
-        self.basel = BaseModel()
-        self.name = "Jocelyn"
-        self.basel.my_number = 30
+    """setup for the test"""
+    def setUpClass(cls):
+        cls.basel = BaseModel()
+        cls.basel.name = "Jocelyn"
+        cls.basel.my_number = 30
 
     @classmethod
-    def tearDown(self):
-        del self.base1
+    def tearDownClass(cls):
+        """at the end of the test this will tear it down"""
+        del cls.basel
         try:
             os.remove("file.json")
-        except FileNotFoundError:
+        except Exception as e:
+            print(e)
             pass
 
     def test_init(self):
-        self.assertTrue(isinstance(self.base1, BaseModel))
+        """test if the base is an instance of type BaseModel"""
+        self.assertTrue(isinstance(self.basel, BaseModel))
 
     def test_atritt(self):
+        """test if the attritt exist"""
         self.assertTrue(hasattr(BaseModel, "__init__"))
         self.assertTrue(hasattr(BaseModel, "to_dict"))
         self.assertTrue(hasattr(BaseModel, "save"))
 
     def test_to_dict(self):
+        """test if to_dictionary method works"""
         base = BaseModel()
         base.name = "Kobe"
         base.age = 40
@@ -44,13 +51,21 @@ class TEST_base(unittest.TestCase):
         self.assertEqual(convert["__class__"], type(base).__name__)
 
     def tets_save(self):
-        self.base1.save()
-        self.assertNotEqual(self.base1.created_at, self.base1.updated_at)
+        """test if the save method works"""
+        self.basel.save()
+        self.assertNotEqual(self.basel.created_at, self.basel.updated_at)
 
     def test_functions(self):
+        """test if the fucntions exits """
         self.assertIsNotNone(BaseModel.__doc__)
         self.assertIsNotNone(BaseModel.save.__doc__)
         self.assertIsNotNone(BaseModel.to_dict.__doc__)
+
+    def test_pep8_BaseModel(self):
+        """test pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/base_model.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
 
 if __name__ == "__main__":
